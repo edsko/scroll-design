e9aca7d5924d02c330e7576c6a722dd9f8e51ac1
Edsko de Vries <edsko@well-typed.com>
2022-08-13 07:54:16 +0200

    Skeleton lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import Text.Blaze.Html.Renderer.Pretty
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes qualified as A
 
+{-------------------------------------------------------------------------------
+  HTML rendering
+-------------------------------------------------------------------------------}
+
+page :: Html
+page = docTypeHtml $
+    return ()
+
+{-------------------------------------------------------------------------------
+  Application driver
+-------------------------------------------------------------------------------}
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn $ renderHtml page
