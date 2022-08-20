d886c83628f585894d3ccfa971d721b021c8d8da
Edsko de Vries <edsko@well-typed.com>
2022-08-20 15:26:29 +0200

    Simplify rendering (depend on CSS more)
    And set the proportions correctly for the Jordan scroll

--- lib/ScrollDesign/AST.hs
+++ lib/ScrollDesign/AST.hs
@@ -1,23 +1,11 @@
 module ScrollDesign.AST (
     Scroll(..)
-  , Pos(..)
-  , Delta(..)
-  , AttrName
-  , AttrValue(..)
   ) where
-
-data Pos = Pos { posX :: Int, posY :: Int }
-
-data Delta = Delta { deltaX :: Int, deltaY :: Int }
-
-type AttrName = String
-
-data AttrValue = Unitless String | WithUnit String String
 
 data Scroll =
     Text String
+  | Div String [Scroll]
   | Elements [Scroll]
-  | SetPos Pos Scroll
   | SetSize Int Scroll
-  | SetAttr AttrName AttrValue Scroll
   | SetLimit [Int] Scroll
+  | SetWiggle Int Scroll
--- lib/ScrollDesign/Render/HTML.hs
+++ lib/ScrollDesign/Render/HTML.hs
@@ -2,12 +2,11 @@
 
 module ScrollDesign.Render.HTML (renderHtml) where
 
-import Prelude hiding (head, div)
+import Prelude hiding (id, head, div)
 
-import Data.String
 import Data.List (intercalate)
-import Data.Map (Map)
-import Data.Map qualified as Map
+import Data.String
+import System.Random.Stateful
 
 import Text.Blaze.Html.Renderer.Pretty qualified as Blaze
 import Text.Blaze.Html5 hiding (map)
@@ -23,74 +22,94 @@ renderHtml scroll = Blaze.renderHtml $ do
              ! A.href "scroll.css"
         title "scroll-design"
       body $
-        div ! A.class_ "container" $
-          renderScroll initRenderCtxt scroll
+        runStateGen_ (mkStdGen 0) $ \gen ->
+          renderScroll gen initRenderCtxt scroll
 
 {-------------------------------------------------------------------------------
   Rendering context
 -------------------------------------------------------------------------------}
 
 data RenderCtxt = RenderCtxt {
-      renderCtxtPos   :: Pos
-    , renderCtxtSize  :: Int
-    , renderCtxtAttrs :: Map AttrName AttrValue
-    , renderCtxtLimit :: [Int]
+      renderCtxtSize   :: Int
+    , renderCtxtLimit  :: [Int]
+    , renderCtxtWiggle :: Int
     }
 
 initRenderCtxt :: RenderCtxt
 initRenderCtxt = RenderCtxt {
-      renderCtxtPos   = Pos { posX = 0, posY = 0 }
-    , renderCtxtSize  = 20
-    , renderCtxtAttrs = Map.empty
-    , renderCtxtLimit = []
+      renderCtxtSize   = 20
+    , renderCtxtLimit  = []
+    , renderCtxtWiggle = 0
     }
 
 {-------------------------------------------------------------------------------
   Renderer proper
 -------------------------------------------------------------------------------}
 
-renderScroll :: RenderCtxt -> Scroll -> Html
-renderScroll ctxt = \case
-    Text t ->
-      renderText ctxt t
-    Elements ks ->
-      mapM_ (renderScroll ctxt) ks
-    SetPos pos k ->
-      renderScroll ctxt{renderCtxtPos = pos} k
-    SetSize size k ->
-      renderScroll ctxt{renderCtxtSize = size} k
-    SetAttr name value k -> do
-      let attrs' = Map.insert name value (renderCtxtAttrs ctxt)
-      renderScroll ctxt{renderCtxtAttrs = attrs'} k
-    SetLimit limit k ->
-      renderScroll ctxt{renderCtxtLimit = limit} k
-
-renderText :: RenderCtxt -> String -> Html
-renderText RenderCtxt{renderCtxtSize = size, ..} =
-    go renderCtxtLimit renderCtxtPos
+renderScroll :: forall g m.
+     StatefulGen g m
+  => g -> RenderCtxt -> Scroll -> m Html
+renderScroll gen = go
   where
-    go :: [Int] -> Pos -> String -> Html
-    go _     _   []     = return ()
+    go :: RenderCtxt -> Scroll -> m Html
+    go ctxt = \case
+        Text t ->
+          renderText gen ctxt t
+        Div id ks ->
+          fmap (\elems -> div ! A.id (fromString id) $ sequence_ elems) $
+            mapM (go ctxt) ks
+        Elements ks ->
+          sequence_ <$> mapM (go ctxt) ks
+        SetSize size k ->
+          go ctxt{renderCtxtSize = size} k
+        SetLimit limit k ->
+          go ctxt{renderCtxtLimit = limit} k
+        SetWiggle wiggle k ->
+          go ctxt{renderCtxtWiggle = wiggle} k
+
+renderText :: forall g m.
+     StatefulGen g m
+  => g -> RenderCtxt -> String -> m Html
+renderText gen RenderCtxt{renderCtxtSize = size, ..} =
+    go renderCtxtLimit (Pos { posX = 0, posY = 0 })
+  where
+    go :: [Int] -> Pos -> String -> m Html
+    go _     _   []     = return $ return ()
     go limit pos (c:cs) = do
-        div ! A.class_ "scrollElement"
-            ! A.style (renderAttrs $ attrs pos) $
-          fromString [c]
-        uncurry go (updatePos limit pos) cs
+        pos' <- wiggle pos
+        let this = do div ! A.class_ "scrollElement"
+                          ! A.style (renderAttrs $ attrs pos') $
+                        fromString [c]
+        (this >>) <$> uncurry go (updatePos limit pos) cs
+
+    wiggle :: Pos -> m Pos
+    wiggle (Pos x y) = do
+        wx <- uniformRM (negate renderCtxtWiggle, renderCtxtWiggle) gen
+        wy <- uniformRM (negate renderCtxtWiggle, renderCtxtWiggle) gen
+        return $ Pos (x + wx) (y + wy)
 
     updatePos :: [Int] -> Pos -> ([Int], Pos)
     updatePos []     (Pos x y) = ([]       , Pos x (y + size))
-    updatePos (1:ls) (Pos x _) = (ls       , Pos (x - size) (posY renderCtxtPos))
+    updatePos (1:ls) (Pos x _) = (ls       , Pos (x - size) 0)
     updatePos (l:ls) (Pos x y) = ((l-1):ls , Pos x (y + size))
 
     attrs :: Pos -> [(AttrName, AttrValue)]
-    attrs (Pos x y) = concat [
-          Map.toList renderCtxtAttrs
-        , [ ("left"   , show x    `WithUnit` "px")
-          , ("top"    , show y    `WithUnit` "px")
-          , ("width"  , show size `WithUnit` "px")
-          , ("height" , show size `WithUnit` "px")
-          ]
+    attrs (Pos x y) = [
+          ("left"   , show x    `WithUnit` "px")
+        , ("top"    , show y    `WithUnit` "px")
+        , ("width"  , show size `WithUnit` "px")
+        , ("height" , show size `WithUnit` "px")
         ]
+
+{-------------------------------------------------------------------------------
+  Internal auxiliary
+-------------------------------------------------------------------------------}
+
+data Pos = Pos { posX :: Int, posY :: Int }
+
+type AttrName = String
+
+data AttrValue = Unitless String | WithUnit String String
 
 renderAttrs :: [(AttrName, AttrValue)] -> AttributeValue
 renderAttrs = fromString . intercalate " " . map (uncurry aux)
