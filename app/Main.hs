6a0c1ae7eb4d57466174d39c2981b9c9a73ca36e
Edsko de Vries <edsko@well-typed.com>
2022-11-19 11:18:58 +0100

    New layout for Kirstin's scroll

--- app/Main.hs
+++ app/Main.hs
@@ -16,22 +16,29 @@ scroll = Div "scroll" [
             SetSize 83
           $ Text "也正在于此"]
 
-      -- 2cm
-    , Div "smallPrint" [
+    , Div "smallPrintTopRight" [
             SetSize 15
-          $ SetLimit [13 + 11, 6 + 9, 6, 13]
-          $ Text $ concat [ "我的职业生涯中投失了九千球"
-                          , "输掉了差不多三百场比赛"
-                          , "还有二十六次"
-                          , "队友把决胜球传给我"
-                          , "结果我没投中"
-                          , "我的人生中失败过一次又一次"
-                          ]
+          $ Text "我的职业生涯中投失了九千球输掉了"
+        ]
+
+    , Div "smallPrintBottomRight" [
+            SetSize 15
+          $ Text "差不多三百场比赛还有二十六"
+        ]
+
+    , Div "smallPrintTopLeft" [
+            SetSize 15
+          $ Text "次队友把决胜球传给我结果我没投中"
+        ]
+
+    , Div "smallPrintBottomLeft" [
+            SetSize 15
+          $ Text "我的人生中失败过一次又一次"
         ]
 
       -- 2cm
     , Div "signature" [
-            SetSize 15
+            SetSize 10
           $ Text "德安书"
         ]
 
