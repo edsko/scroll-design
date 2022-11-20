c1a9c4d909a5fd8cb980f613063f85144e0738c4
Edsko de Vries <edsko@well-typed.com>
2022-11-20 08:54:41 +0100

    Scroll redesign for Kerstin

--- app/Main.hs
+++ app/Main.hs
@@ -5,44 +5,49 @@ import ScrollDesign
 
 scroll :: Scroll
 scroll = Div "scroll" [
-      -- 13cm
-      Div "focalPoint" [
-            SetSize 97
-          $ Text "我成功的原因"
-        ]
 
-      -- 11cm
-    , Div "greenText" [
-            SetSize 83
-          $ Text "也正在于此"]
+      Div "fabricTop" []
 
-    , Div "smallPrintTopRight" [
-            SetSize 15
-          $ Text "我的职业生涯中投失了九千球输掉了"
-        ]
+    , Div "scrollBody" [
 
-    , Div "smallPrintBottomRight" [
-            SetSize 15
-          $ Text "差不多三百场比赛还有二十六"
-        ]
+        Div "focalPoint" [
+              SetSize 100
+            $ Text "我成功的原因"
+          ]
 
-    , Div "smallPrintTopLeft" [
-            SetSize 15
-          $ Text "次队友把决胜球传给我结果我没投中"
-        ]
+      , Div "greenText" [
+              SetSize 70
+            $ Text "也正在于此"]
 
-    , Div "smallPrintBottomLeft" [
-            SetSize 15
-          $ Text "我的人生中失败过一次又一次"
-        ]
+      , Div "smallPrintTopRight" [
+              SetSize 15
+            $ Text "我的职业生涯中投失了九千球输掉了"
+          ]
 
-      -- 2cm
-    , Div "signature" [
-            SetSize 10
-          $ Text "德安书"
-        ]
+      , Div "smallPrintBottomRight" [
+              SetSize 15
+            $ Text "差不多三百场比赛还有二十六"
+          ]
 
-    , Div "stamp" []
+      , Div "smallPrintTopLeft" [
+              SetSize 15
+            $ Text "次队友把决胜球传给我结果我没投中"
+          ]
+
+      , Div "smallPrintBottomLeft" [
+              SetSize 15
+            $ Text "我的人生中失败过一次又一次"
+          ]
+
+      , Div "signature" [
+              SetSize 10
+            $ Text "德安书"
+          ]
+
+      , Div "stamp" []
+      ]
+
+    , Div "fabricBottom" []
     ]
 
 main :: IO ()
